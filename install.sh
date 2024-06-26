c96827c53f2a7a5bcc3c4cd2e5773762c13895a2
Michele Bono <bono.michele94@gmail.com>
2024-06-26 09:06:25 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt install snapd
+snap install core
+snap install ubuntu-frame mesa-core20 ubuntu-frame-osk wpe-webkit-mir-kiosk
+snap set wpe-webkit-mir-kiosk url=http://10.250.10.45/login.aspx?lang=ROM
+snap set ubuntu-frame daemon=true
+snap set ubuntu-frame-osk daemon=true
+snap set wpe-webkit-mir-kiosk daemon=true
+mkdir -p /var/snap/ubuntu-frame-osk/common/squeekboard/keyboards/
